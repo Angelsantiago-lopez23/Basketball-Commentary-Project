46389091cfc44ea0613890ac96c755ddfcda7adb
Angelsantiago-lopez23 <80490972+Angelsantiago-lopez23@users.noreply.github.com>
2021-03-25 22:52:05 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,200 @@
+def main():
+    # Angel Santiago-Lopez, python program is a high-level based program and is one of the easiest to learn as a starter.
+    # The program is object oriented and this project will revolve around a sports theme.
+
+    print("Hello, welcome to a mini basketball commentary of a Brooklyn Nets vs Los Angeles Lakers game")
+    print(
+        "Ball is tipped off and 3 minutues in no one has scored.\nLebron goes down the court for a layup, gets fouled but the basketball is midair and swirls around the rim for and one")
+    print("2+1:", "3 points for the team after the made the freethrow")
+    # this addition of string was to show the reaction from the team/excitement
+    print("LETSSS" + "GOOOOO!!!")
+    print("The teams gets hyped up with the crowd")
+    print(
+        "The opposing star player named Durant pulls up the court and shoots a 3.\nDurant steals the ball and scores another 3")
+    print(6 - 1)
+    print("5 is his total points rather than 6 due to the 2nd three being a far 2 pointer")
+    print("As the game intensifies Lebron throw the ball up in the air for a easy dunk for his teammate AD")
+    print(6 / 3)
+    print("2 points is added to the score.\nThe first quarter ends")
+    print(2 * 3, " points are added in the first minute for Brooklyn in the second quarter")
+    print(7 // 3, "points after Lebron mid range shot goes in")
+    print(7 % 3,
+          "point added to the Lakers team after the Brooklyn bench was hit with a technical foul due to trash talk")
+    print(2 ** 2, "points added after Kyle Kuzma was fouled at the three and made the contested shot and free throw")
+    print(
+        "As the game came to half time, Kyrie did a euro step to slowly make space for himself to the rim.\n4\n3\n2\n1\nThe buzzer went off but Kyrie got his shot just in time for 2 points.")
+    singername = input("Your favorite artist to perform for halftime? ")
+    print("At halftime the artist to perform is", singername)
+    print(
+        "After the artist performance the third quarter started off with a\nbanger\nAs Kevin Durant ran past the defenders for a thunder slam dunk")
+    # determine the moves Lebron takes to the rim
+    moves = input("Lebron spins or backstep?: ")
+    face = input("Facial expresion?: ")
+    print("Lebron drives to the paint " + moves, "with an " + face, "face and the crowd cheers")
+    print("The crowd stands up after the sequence of moves Lebron has done")
+    print("The crowd surges Lebron to repeadtly do it again and again")
+    print(
+        "As the Lakers team go on a 20-0 run, the opposing team calls a timeout\n Times ticks as the 3rd quarter is almost over")
+    print(
+        "Once third quarter finishes, the last quarter is mostly both teams hitting back and forth shots but the Lakers with the advantage still")
+    lbjpoints = 36
+    print("3 minute in the 4th quarter, Lebron has", lbjpoints)
+    chantsppl = "Lebron!" * 6
+    print(chantsppl)
+    print("The chants from the crowd simply added fuel to the Broklyn Nets team as they went on 14-0 run")
+    crowd2chant = "Defense!" * 10
+    print(crowd2chant)
+    print("This is crowd reaction to stop the Nets from scoring after their run since they were 2 points away")
+    print(
+        "AD was foulded at the 3 pointer line but the referee don't know whether to count it as a 2 pointer or 3 pointer")
+    # determine if 3 or 4 total points
+    optionone = input(" Is it 4 points total or 3 points total?: ")
+    optiontwo = input("3 pointer or 2 pointer: ")
+    num1 = int(optionone)
+    num2 = int(optiontwo)
+    difference = num1 - num2
+    print("In the end it was a ", difference)
+    print("This was the amount of freethrows he took after the 3 pointer or 2 pointer decision")
+    print(
+        "With 2 minutes remaining and a tie score between both teams, their movement has slowed down to slowly take away time")
+    crowd3 = "cricket" * 11
+    print(crowd3)
+    print("The silence of the arena shows how focused the fans are at the play by play by both teams")
+    print(
+        "As the seconds go by to just 10 seconds with Lebron holding the ball.\nThey deploy a screen for Lebron to be free from the defenders but the opposing team react quickly to guard him")
+    print(
+        "Time slows down and Lebron crosses over Kyrie and spins toward the rim.\n5\n4\n3\n2\n1\nBzzzz goes off the timer and Lebron makes the shot at the buzzer")
+    finalcrowd = "Roarrrrr" * 24
+    print(finalcrowd)
+    print("The crowd screams after the final play by Lebron and the victoy of the Los Angeles Lakers Team")
+    lakersteam = "AHHHHHHHHHHH" * 10
+    print(lakersteam)
+    print(
+        "The Lakers teams are screaming with the fans and Lebron after the play and the Broklyn Nets slowly head back to the locker room")
+    print("The match finishes on a 104-102 score game and the media comes out for the interview")
+    print(
+        "And this is Angel Santiago-Lopez, the commentator for today's game. What a wild finish to today's match. Have a great day!!!")
+#2
+    #Several Weeks pass and this is the start of the NBA Finals with the Los Angeles Lakers against the Brooklyn Nets
+    print("Welcome this is your host Angel and today's game is Game 1 of the NBA Finals")
+    print("Both teams are fired up!! \nBoth seek the big prize at the end of the road.")
+    print("Let's take a look st the audience prediction")
+    bb00_fan1 = 50
+    bb01_fan2 = 49
+    if bb01_fan2 > bb00_fan1:
+        print("Los Angeles Lakers has the higher support from the audience")
+    else:
+        print("The Brooklyn Nets are chosen as the winner overall by the audience")
+    print("As we can see from the result, Brooklyn Nets are the favorites in wining tonight's game")
+    print("The Lakers get the first touch at the start and both team seem hungry for the win")
+    print("After a few minutes in the game the Lakers team have roughly 26 points but needs 2 points added after Lebron's dunk")
+    lakers_firstqscore = 28
+    if (lakers_firstqscore > 20) and (lakers_firstqscore <30):
+        print("Lakers and the Brooklyn are close in points")
+    else:
+        print("Brooklyn have a greater amount of points in the game and are ahead")
+    print("About 10 minutes within the first quarter it has become neck to neck between both teams as they trade buckets back and forth")
+    print("As the quarter almost ends, Kevin Durant dribbled right through the defenders for an easy layup. \nLeaving them frustrated for their late reaction on getting back on defense.")
+    print("Quarter 2 starts with Kuzma hitting a 3 with his eyes closed")
+    lakerspts = 29
+    broklynpts = 29
+    if lakerspts == broklynpts:
+        print("Both teams tied at 29 points after Kuzma 3")
+    audience = "Defense" * 10
+    print(audience)
+    print("The Lakers crowd chanted defense until it was their possession")
+    print("During a timeout by the Brooklyn Nets, the crowd had a chance to win a ticket to the after party if they score more than 20 points in a mini basketball between 2 selected fans")
+    print("Either score more than 20 points or \n ....... Score at least 15 points for just a free meal")
+    fan_number1 = 18
+    if fan_number1 > 20 or fan_number1 >= 15:
+        print("Congrats you qualified for the free ticket or the free meal if scored at least 15 points")
+    else:
+        print("You lost the chance for a free ticket, not your day bro")
+    print("Wow, just shy of 3 points of getting more than 20 points for the free ticket. \n Let's see if the second contestant is able to win it.")
+    fan_number2 = 21
+    if fan_number2 > 20 or fan_number2 >= 15:
+        print("Congrats you qualified for the free ticket or the free meal if scored at least 15 points")
+    else:
+        print("You as well lost the chance for a free ticket, a sad day for our fellow people")
+    print("Fan number 2 was able to win the free ticket and as well the free meal. \n Congrats to both contestants on the well played game")
+    print("As the 2nd quarter resumes, Anthony Davis caught fire as he went on a 9-9 field goals made in 3 minutes")
+    print("Which in turn cause for another timeout for the Brooklyn team to revise the game-plan and stop the momentum")
+    print("Both teams slowly return back to the court and its Lakers possession. \n Lebron slowly dribbles up the court but is confronted by Kevin Durant.")
+    print("The crowd slowly rises as many fans want a KD vs LeBron match up.")
+    audiencept2 = "Booo!" * 3
+    print(audiencept2)
+    print("The crowd Booed at Lebron since this was a home game at the Brooklyn Stadium and many fans don't want to see a back to back win in the finals for the LA team")
+    print("As the time slowly runs down, LeBron turns his back against KD to give him space and Harrell comes for a screen.")
+    print("LeBron uses this to make room for him and sprints past the Nets defenders for a _____")
+    lbjmoveanswer = input("What motion does LeBron do on his way to the rim?")
+    if lbjmoveanswer == "dunks":
+        print("Sprints past the Nets defenders for a tomahawk dunk on Harden as he attempts to strip the ball away.")
+    elif lbjmoveanswer == "spins":
+        print("Sprints past the Nets defenders and spins around Harden for a buttery layup")
+    elif lbjmoveanswer == "fadeaway":
+        print("Sprints past the Nets defenders and does a back step for a fade-away shot")
+        #If not one of the 3 options then I don't know what to say. Need to be flashy with the ball.
+    else:
+        print("LeBron dances with the ball, which results in a travel for holding the ball and dancing")
+    print("The crowd goes silent as what Lebron has done to the Brooklyn defenders but the Lakers bench are going crazy")
+    lakerbench = "LETSSS GO BRON" * 3
+    print(lakerbench)
+    print("As quarter 2 comes to an end, Lakers are ahead in points. ")
+    #Coach Vogel is the LA coach and for the Nets the coach is Steve Nash
+    print("In the Lakers area we here that Coach Vogel hasn't paid much attention to the score board and judging by the amount of points ahead will determine his mood.")
+    lakerslead = 20
+    if lakerslead >= 10:
+        print("Amazing great defense and offense has lead us to this 20 point advantage said Coach Vogel")
+    print("During the halftime, there were shows performed by dancers and artist's such as Kendrick Lamar and Metallica who performed their hit songs.")
+    print("What a great performance by these talented people but it seems that the fans are interested in Magic Johnson playing rock,paper, and scisors with one of the crowd.")
+    print("As the camera zoom in we can hear the conversation between the two.")
+    print("Magic says rock, paper, and scissors")
+    magicopponent = input('type "rock", "paper", or "scissors"')
+    if magicopponent == "rock":
+        print("paper, nice try kid")
+    if magicopponent == "paper":
+        print("scissors, nice try kid")
+    if magicopponent == "scissors":
+        print("rock, better luck next time")
+    print("The crowd cheers for the kid but ultimately loses to Magic, both shake hands as a sign of good match between the two")
+    print("As the crowd slowly quites down, the player make their way back to the court as quarter 3 begins")
+    audiencept3 = "AHHHHHHHH" * 2
+    print(audiencept3)
+    print("The crowd amps up as both teams start of strong. \n Both teams going strong without a miss and the crowd awing as multiple players dunk.")
+    print("Kryie is showing of the skills package and has the whole crowd amazed by his handles")
+    print("Kyrie slowly creeps up to for a layup but is blocked by KCP and throws a half court pass to Gasol for a wide open three.")
+    print("The crowd begins to chant")
+    audiencept4 = input("What do the crowd chant?: ")
+    x = 0
+    while(x<15):
+        print(audiencept4)
+        x = x + 1
+    print("Resulting in both teams to fire up once again, ending the 3rd quarter with a score of 89-95. \n With LA ahead by 6 points.")
+    print("Fourth quarter starts off in the hand of the Nets and the trio of Harden, Kyrie and Durant go on a 15 point run.")
+    print("WOW! The trio are going off, the crowd begins to yell")
+    audiencept5 = input("What do the crowd yell after the trio going off?: ")
+    for x in range(10):
+        print(audiencept5)
+    print("But the Lakers responded with a tough lockdown on the Nets and scoring back to back threes to tie the game.")
+    print("The Lakers than begin to respond with multiple shots and the Nets as well. \nBack\nand Forth\nBack\nand Forth\nBack\n and Forth")
+    print("Till there was just 3 minutes left on the score board")
+    print("During a timeout by the Nets, a fan had earned about 16.8 points in a mobile game that has earned him a free meal")
+    print("The points were than eventually rounded up.")
+    print(round(16.8))
+    print("Rounded up to 17 points but then the game would resume right after. \n With Denis hitting a floated to give the Lakers a lead")
+    print("As it comes to the last minute, the crowd becomes quiet and stand as the minutes comes down to seconds.")
+    print("Durant pulls up for a clutch 3 but then AD proceeds to hit a 3 pointer as well. \nGiving LA a one point lead.")
+    print("There is 10 seconds left and the Lakers were able to strip the ball and Gasol passes it to Lebron for a windmill dunk.")
+    audiencept6 = "Silence" * 4
+    print(audiencept6)
+    print("This was the reaction of the Nets crowd as they know the Lakers has won the game with one second left.")
+    print("Time runs out and the Lakers takes the fist win home. With the trio leaving quickly to the lockers in frustration.")
+    print("The media runs out and this is Angel your commentator for today and I'll be seeing you next game!!")
+main()
+
+
+
+
+
+
+
